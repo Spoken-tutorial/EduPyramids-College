c5bd3465cd6ba1901d242d36bb5de57bb5165e35
ankitamk14 <ankitamk@gmail.com>
2025-10-31 10:35:09 +0530

    payments app

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework_simplejwt",
-    "users"
+    "users",
+    "payments",
 ]
 
 MIDDLEWARE = [
