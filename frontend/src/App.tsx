250c8d8888b7a662ab3876952073ba39df82ee27
Naman Sharma <namansharma567891@gmail.com>
2025-11-06 12:41:11 +0000

    added subscription page and updated the external links in the navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,12 @@
 
 import { Routes, Route } from "react-router-dom";
-import ResponsiveAppBar from "./components/homepage/ResponsiveAppBar";
 import MegaMenu from "./components/homepage/AppBar";
 // import FeatureTiles from "./components/homepage/HomeComponents";
 import HomePage from "./pages/home/Homepage";
 import DomainPage from "./pages/public/DomainsPage";
 import CoursePage from "./pages/public/CoursePage";
 import TutorialSearch from "./pages/public/TutorialSearch";
+import SubscriptionPage from "./pages/public/SubscriptionPage";
 
 
 export default function App(){
@@ -22,6 +22,7 @@ export default function App(){
           <Route path="/domains" element={<DomainPage />} />
           <Route path="/tutorial-search" element={<TutorialSearch />} />
           <Route path="/domains/:slug" element={<CoursePage />} />
+          <Route path="/subscription" element={<SubscriptionPage />} />
           {/* catch-all for 404 */}
           <Route path="*" element={<h1>Page Not Found</h1>} />
         </Routes>
